bf7cecd6dd0b57b865405a1dce0386497065028a
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2022-07-15 02:15:58 +0000

    Fix bad code

--- src.lua
+++ src.lua
@@ -1,9 +1,8 @@
 --- Script-Ware Script Dumper by nul#3174 ---
 
-if not identifyexecutor then return end
+if not identifyexecutor or identifyexecutor() ~= "ScriptWare" then return end
 
-if identifyexecutor and identifyexecutor() ~= "ScriptWare" then return end
-
+local runService = game:GetService("RunService")
 local savefolderas = makefolder
 local savefileas = writefile
 local folderexists = isfolder
@@ -14,10 +13,10 @@ local tbl = table
 local tbl_insert = tbl.insert
 local mth = math
 local mth_random = mth.random
-local mth_randomseed = mth.randomseed
 local tsk = task
 local tsk_wait = tsk.wait
 local foldername = string.gsub(game:GetService("MarketplaceService"):GetProductInfo(placeid).Name, "[^%w%s]", "")
+mth.randomseed(tick())
 
 local function decompile(a)
     if a:IsA("ModuleScript") or a:IsA("LocalScript") then
@@ -30,15 +29,14 @@ local function decompile(a)
 end
 
 local function randomNumbers()
-    mth_randomseed(tick())
-    game:GetService("RunService").RenderStepped:Wait()
+    runService.RenderStepped:Wait()
     return tostr(mth_random(1e9, 2e9))
 end
 
 local scripts = {}
 local out
 
-for i, v in next, game:GetDescendants() do
+for i, v in next, getscripts() do
     if v:IsA("ModuleScript") or v:IsA("LocalScript") and not v:FindFirstAncestor("CoreGui") and not v:FindFirstAncestor("CorePackages") then
         if not v:FindFirstAncestor("Chat") and not v:FindFirstAncestor("PlayerModule") then
             tbl_insert(scripts, v)
@@ -47,15 +45,13 @@ for i, v in next, game:GetDescendants() do
 end
 
 for i, v in next, scripts do
+	runService.RenderStepped:Wait()
 
-game:GetService("RunService").RenderStepped:Wait()
+	out = decompile(v)
 
-out = decompile(v)
+	if not folderexists(tostr(foldername.."'s Script Dump Output")) then
+		savefolderas(tostr(foldername.."'s Script Dump Output"))
+	end
 
-if not folderexists(tostr(foldername.."'s Script Dump Output")) then
-    savefolderas(tostr(foldername.."'s Script Dump Output"))
-end
-
-savefileas(tostr(foldername.."'s Script Dump Output".."/"..v.Name..randomNumbers()..".lua"), tostr(out))
-
+	savefileas(tostr(foldername.."'s Script Dump Output".."/"..v.Name..randomNumbers()..".lua"), tostr(out))
 end
